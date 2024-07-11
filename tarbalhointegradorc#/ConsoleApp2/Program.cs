6d5f5734aa3d0b6df654853aba18ef3f0cea099c
guedert <162443425+guedertt@users.noreply.github.com>
2024-07-11 17:03:52 -0300

    Add files via upload

---
+++ tarbalhointegradorc#/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+
+    {
+        bool opcoes = false;
+
+        Console.WriteLine("Olá professor!, digite 1 para adicionar uma nova matéria ou digite 2 para avaliar os alunos em sua matéria.");
+        int input = int.Parse(Console.ReadLine());
+
+        switch (input)
+        {
+            case 1:
+                opcoes = true;
+                break;
+            case 2:
+                break;
+        }
+       
+        string escolha;
+        while (opcoes == true)
+        {
+            Disciplinas nova_materia = new Disciplinas();
+            Console.WriteLine("escreva o nome da matéria");
+            nova_materia.Nome = Console.ReadLine();
+            Console.WriteLine(nova_materia.Nome);
+            Console.WriteLine("voce quer digitar uma nova materia? (s/n)");
+            escolha = Console.ReadLine();
+
+            if (escolha == "s")
+            {
+                continue;
+            }
+            else if (escolha == "n")
+            {
+                opcoes = false;
+            }
+            else
+            {
+                Console.WriteLine("digite s ou n");
+            }
+            Console.WriteLine($"qual o peso da materia {nova_materia.Nome}");
+            
+        }
+    }
+}
