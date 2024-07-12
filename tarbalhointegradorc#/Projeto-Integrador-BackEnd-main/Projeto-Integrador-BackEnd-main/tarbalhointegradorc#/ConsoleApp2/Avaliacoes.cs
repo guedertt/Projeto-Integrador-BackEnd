45200a48566aa3ee54b43b63eada5a613fb8550e
guedert <162443425+guedertt@users.noreply.github.com>
2024-07-11 21:04:21 -0300

    Add files via upload

---
+++ tarbalhointegradorc#/Projeto-Integrador-BackEnd-main/Projeto-Integrador-BackEnd-main/tarbalhointegradorc#/ConsoleApp2/Avaliacoes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Avaliação
+    {
+        #region atribuitos
+        public int Peso { get; set; }
+
+        public int Nota { get; set; }
+
+        public string AlunoNome { get; set; }
+        #endregion
+    }
+
+}
