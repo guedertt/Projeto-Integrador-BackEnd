627eed3700dc42743003581b186b47210ac880b5
guedert <162443425+guedertt@users.noreply.github.com>
2024-07-16 16:59:01 -0300

    Add files via upload

---
+++ versao 2.0 trabalho integrador/Class1.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Disciplinas
+    {
+        #region atribuitos
+        public string Nome { get; set; }
+        public List<Avaliação> Avaliacoes { get; set; }
+        #endregion
+        public static void AdicionarMateria()
+        {
+            Disciplinas nova_materia = new Disciplinas();
+
+            Console.WriteLine("Digite o nome da matéria:");
+            nova_materia.Nome = Console.ReadLine();
+            Alunos.AvaliarAlunos();
+        }
+    }
+}
---
+++ versao 2.0 trabalho integrador/Program.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleApp2;
+using ConsoleApp3;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool opcoes = true;
+        bool opcoes2 = true;
+
+        while (opcoes)
+        {
+            Console.WriteLine("Olá professor! Digite 1 para adicionar uma nova matéria ou 2 para avaliar os alunos em sua matéria.");
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    Disciplinas.AdicionarMateria();
+                    opcoes = false;
+                    break;
+
+                case 2:
+                    Alunos.AvaliarAlunos();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Digite 1 ou 2.");
+                    break;
+            }
+        }
+        while (opcoes2) { 
+             Console.WriteLine("Você quer adicionar outro aluno para a mesma matéria? (1 para sim, 2 para não)");
+             int input2 = int.Parse(Console.ReadLine());
+
+            switch (input2)
+            {
+                case 1:
+                    Alunos.AvaliarAlunos();
+                    break;
+                case 2:
+                    Alunos.CalcularMedia();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Digite 1 ou 2.");
+                    break;
+            }
+        }     
+    }
+}
